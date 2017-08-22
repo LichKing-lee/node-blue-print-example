176ea0632d9cb60ca1c23ad8275bdbbee022de4a
ChangYong <lcy9002@naver.com>
2017-08-22 20:09:55 +0900

    user controller add

---
+++ ch-02/controllers/user.js
@@ -0,0 +1,18 @@
+var models = require('../models/index');
+var User = require('../models/user');
+
+exports.create = function(req, res){
+    models.User.create({
+        name: req.body.name,
+        email: req.body.email
+    }).then(function(user){
+        res.json(user);
+    });
+};
+
+exports.list = function(req, res){
+    models.User.findAll({})
+        .then(function(users){
+            res.json(users);
+        });
+};
